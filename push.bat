faa3a885adc2f71f9f9bdfac0fc4108b773e2669
svddevelop <svddevelop@gmail.com>
2024-01-03 11:01:54 +0100

    clearing

---
+++ push.bat
@@ -0,0 +1,10 @@
+del circuit\kicad\carcontroltool-nano-plan\carcontroltool-esp32-plan.xml
+del circuit\kicad\carcontroltool-nano-plan\*.lck
+del circuit\kicad\carcontroltool-nano-plan\carcontroltool-esp32-plan-backups\*
+@if "%1" EQU "" (
+@echo !!! Add a comment like: push.bat "test x" 
+exit
+)
+git add *
+git commit -m %1
+git push origin esp32
